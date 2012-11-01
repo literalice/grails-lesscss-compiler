0e2b988b7dc2b5e3d39b2790906a796817c62ec7
literalice <literalice@monochromeroad.com>
2012-11-01 13:17:03 +0900

    adds a less compiler class

---
+++ scripts/_LessCompile.groovy
@@ -0,0 +1,51 @@
+import groovy.io.FileType
+import org.lesscss.LessCompiler
+import org.lesscss.LessException
+
+target(compileLess: "Compiles all of the less css files.") {
+    compileLessCss()
+}
+
+//noinspection GroovyMissingReturnStatement
+compileLessCss = {
+    def baseDir = grailsSettings.baseDir as File
+    def lessDir = new File(baseDir, "src/less")
+    LessCssCompiler lessCssCompiler = new LessCssCompiler(baseDir)
+    if (lessDir.canRead()) {
+        lessDir.eachFileRecurse(FileType.FILES) {
+            lessCssCompiler.compile(it)
+        }
+    }
+}
+
+/**
+ * @author Masatoshi Hayashi
+ */
+class LessCssCompiler {
+
+    private File baseDir
+    private LessCompiler lessCompiler = new LessCompiler()
+
+    LessCssCompiler(File baseDir) {
+        this.baseDir = baseDir
+    }
+
+    File compile(File resource) {
+        File target = new File(generateCompiledFileFromOriginal(resource.absolutePath))
+        try {
+            lessCompiler.compile resource, target
+            return target
+        } catch (LessException e) {
+            System.err.println(e.message)
+            return null
+        }
+    }
+
+    private String generateCompiledFileFromOriginal(String original) {
+        String orgDirPath = /${baseDir.absolutePath}${File.separator}src${File.separator}less${File.separator}/
+        String targetDirPath = /${baseDir.absolutePath}${File.separator}web-app${File.separator}css${File.separator}/
+        original.replace(orgDirPath, targetDirPath).replaceAll(/(?i)\.less/, '_less.css')
+    }
+
+}
+
